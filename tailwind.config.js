e5f7c34f89eca0ab69ef0c0fbd523c749acc4357
Tazim-01 <tx1trx@gmail.com>
2024-03-11 01:35:10 +0600

    new e

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ export default {
       },
       
       backgroundImage: {
-        'hero-image': "url('./portfolioimage/banner.jpg')",
+        'hero-image': "url('./banner.jpg')",
 
       }
     },
